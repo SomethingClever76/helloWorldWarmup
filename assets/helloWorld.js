23a80d0956cbf94867f79fa9e812986506c3ff72
Thomas <thmasky@amazon.com>
2019-10-15 10:40:24 -0400

    Initial Commit

---
+++ assets/helloWorld.js
@@ -0,0 +1,5 @@
+
+    let header = document.createElement("h1");
+    let text = document.createTextNode("Hello World");
+    header.appendChild(text);
+    document.getElementById("hello").appendChild(header);
