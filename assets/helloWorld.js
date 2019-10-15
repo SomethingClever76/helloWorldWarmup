4c5587ea9010c82dac511f70e1aa594f62d5f635
Thomas <thmasky@amazon.com>
2019-10-15 11:39:10 -0400

    added comments

--- assets/helloWorld.js
+++ assets/helloWorld.js
@@ -1,5 +1,14 @@
-
+   //create variable linked to the div with an id of "hello" in the HTML
+   let warmupDiv = document.getElementById("hello");
+        
+   //create a new h1 element
     let header = document.createElement("h1");
+      
+   //create new text string
     let text = document.createTextNode("Hello World");
+       
+   //add the text string to the h1
     header.appendChild(text);
-    document.getElementById("hello").appendChild(header);
+
+   //add the h1 (which now includes text) to the div 
+    warmupDiv.appendChild(header);
